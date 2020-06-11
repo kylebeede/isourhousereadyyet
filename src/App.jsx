4a671133709090ff87effc7db64e5ab058eeaee4
Kyle Beede <kyle.beede@faithlife.com>
2020-06-11 15:19:13 -0700

    Use target date

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,13 @@ import useScreenDimensions from './useScreenDimensions';
 import './App.css';
 
 const targetDate = new Date('June 29, 2020 00:00:00');
-const testDate = new Date('June 11, 2020 14:54:00');
 
 function App() {
   const { screenWidth, screenHeight } = useScreenDimensions();
 
   return (
     <div className="container" style={{width: `${screenWidth}px`, height: `${screenHeight}px`}}>
-      <Countdown targetDate={testDate} />
+      <Countdown targetDate={targetDate} />
     </div>
   );
 }
